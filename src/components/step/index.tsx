54349c4e94e5fd5c1dfb37396174e7726f39fd31
brunotxrs <elielsonbrunno@gmail.com>
2024-12-15 08:45:29 -0400

    feat: adding the props parameter.

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -3,12 +3,12 @@ import {Text, View } from "react-native"
 import { colors } from "@/styles/theme"
 import { s } from "./styles"
 
-export function Step(){
+export function Step(props){
     return(
         <View style={s.container}>
             <View style={s.details}>
-            <Text style={s.title}>Titulo</Text>
-            <Text style={s.description}>Descrição ...</Text>
+            <Text style={s.title}>{props.title}</Text>
+            <Text style={s.description}>{props.description}</Text>
             </View>
         </View>
     )
