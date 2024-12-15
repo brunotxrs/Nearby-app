cfac603f5b2a638701a09d0dbc356df02d0ecedd
brunotxrs <elielsonbrunno@gmail.com>
2024-12-15 18:37:49 -0400

    feat: importing the api

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Alert } from "react-native";
+
+import { api } from "@/services/api";
 
 export default function Home(){
-    return(
-        <View style={{ flex: 1 }}>
-            <Text>Home</Text>
-        </View>
-    )
+    const [categories, setCategories] = useState()
+
+
+    async function fetchCategories() {
+        try {
+            const { data } = await api.get("/categories")
+            setCategories(data)       
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Categorias", "Não foi possível carregar as categorias")
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+    },[])
+
+    return(<View style={{ flex: 1 }}></View>)
 }
