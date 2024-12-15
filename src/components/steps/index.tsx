5ec24223e0441552b68ecd827c9510ff850273c3
brunotxrs <elielsonbrunno@gmail.com>
2024-12-15 09:54:24 -0400

    feat: updating title and description information

--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -8,7 +8,14 @@ export function Steps(){
         <View style={s.container}>
             <Text style={s.title}>Veja como funciona:</Text>
             
-            <Step/>
+            <Step title="Encontre estabelecimentos"
+            description="Veja locais perto de você que são parceiros Nearby"/>
+
+            <Step title="Ative o cupom com QR Code"
+            description="Escaneie o código no estabelecimento para usar o benefício"/>
+            
+            <Step title="Garanta vantagens perto de você"
+            description="Ative cupons onde estiver, em diferentes tipos de estabelecimento"/>
         </View>
     )
 }
