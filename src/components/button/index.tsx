8369eac94d3d6d8949c8286c5f34f6ba429a841a
brunotxrs <elielsonbrunno@gmail.com>
2024-12-15 16:40:38 -0400

    feat: button component completed

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,19 +1,49 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, TextProps } from "react-native"
+import { 
+    TouchableOpacity,
+    TouchableOpacityProps,
+    Text,
+    TextProps,
+    ActivityIndicator
+} from "react-native"
+import { IconProps as TableIconProps } from "@tabler/icons-react-native"
 
 import { s } from "./styles";
 import { colors } from "@/styles/theme";
 
-function Button({ children }: TouchableOpacityProps){
-    // TouchableOpacity ele faz o que coloca dentro dele uma area de toque ele faz o efeito de opacidade
-    return( <TouchableOpacity style={s.container} activeOpacity={0.6}>{children}</TouchableOpacity>
+type ButtonProps = TouchableOpacityProps & {
+    isLoading?:boolean
+}
 
+function Button({ children, style, isLoading = false, ...rest }: ButtonProps){
+    return( 
+    <TouchableOpacity 
+    style={[s.container, style ]} 
+    activeOpacity={0.6}
+    disabled={isLoading}
+    {...rest}
+    >
+        {isLoading ? (
+            <ActivityIndicator size={"small"} color={colors.gray[100]} />
+        ) : (
+            children
+            )}
+    </TouchableOpacity>
     )
 }
+
 function Title({children}: TextProps){
     return(<Text style={s.title}>{children}</Text>)
 }
 
+type IconProps = {
+    icon: React.ComponentType<TableIconProps>
+}
+
+function Icon( {icon: Icon}: IconProps){
+    return( <Icon size={24} color={colors.gray[100]} />
+ )}
 
 Button.Title = Title
+Button.Icon = Icon
 
 export  { Button }
