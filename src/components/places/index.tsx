4fbbd7c1fd8f796cf1dfb96e717023961433e27a
brunotxrs <elielsonbrunno@gmail.com>
2024-12-16 16:25:07 -0400

    feat: creating the structure and styles of the component, from places

---
+++ src/components/places/index.tsx
@@ -0,0 +1,13 @@
+import {View } from "react-native"
+import { s } from "./styles"
+type Props = {
+    data: []
+}
+
+export function Place({ data }: Props){
+    return (
+        <View>
+            
+        </View>
+    )
+}
