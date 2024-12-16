f5ba5e4e815ec7c19b4f186c5aea06e9290bcd45
brunotxrs <elielsonbrunno@gmail.com>
2024-12-16 15:57:31 -0400

    feat: creating the path to be called a category

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -7,11 +7,13 @@ import { Categories, CategoriesProps } from "@/components/categories";
 
 export default function Home(){
     const [categories, setCategories] = useState<CategoriesProps>([])
+    const [category, setCategory] = useState("")
 
     async function fetchCategories() {
         try {
             const { data } = await api.get("/categories")
-            setCategories(data)       
+            setCategories(data)
+            setCategory(data[0].id)    
         } catch (error) {
             console.log(error)
             Alert.alert("Categorias", "Não foi possível carregar as categorias")
