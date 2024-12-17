87ec417cdf428890a270cfbd7e3c3dfb61c5c0b0
brunotxrs <elielsonbrunno@gmail.com>
2024-12-17 17:56:49 -0400

    feat: importing the cover component

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 import { Alert, View } from "react-native";
-import { router, useLocalSearchParams } from "expo-router"
+import { router, useLocalSearchParams, Redirect } from "expo-router"
 
 import { Loading } from "@/components/loading"
+import { Cover } from "@/components/market/cover";
+import { PropsDetails } from "@/components/market/details";
 
 import { api } from "@/services/api"
 
+type DataProps = PropsDetails & {
+cover: string
+}
+
 export default function Market(){
-    const [data, setData] = useState()
+    const [data, setData] = useState<DataProps>()
     const [isLoading, setIsLoading] = useState(true)
 
     const params = useLocalSearchParams<{ id: string }>()
@@ -39,9 +45,13 @@ export default function Market(){
         )
     }
 
-    return (
-        <View style={{ flex: 1, justifyContent:"center", alignItems:"center" }}>
+    if(!data){
+        return(<Redirect href="/home" />)
+    }
 
+    return (
+        <View style={{ flex: 1 }}>
+            <Cover uri={data?.cover} />
         </View>
     )
 }
