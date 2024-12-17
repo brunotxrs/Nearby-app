21eafa8b2aa2e2151d75ceaeb59f570bdfb873ce
brunotxrs <elielsonbrunno@gmail.com>
2024-12-17 17:29:51 -0400

    feat: updating cover component styles

--- src/components/market/cover/styles.ts
+++ src/components/market/cover/styles.ts
@@ -2,6 +2,14 @@ import { StyleSheet } from "react-native";
 import { colors } from "@/styles/theme";
 
 export const s = StyleSheet.create({
-    container: {},
-    header: {},
+    container: {
+        width:"100%",
+        height: 232,
+        marginBottom: -32,
+        backgroundColor: colors.gray[200],
+    },
+    header: {
+        padding: 24,
+        paddingTop: 56,
+    },
 })
