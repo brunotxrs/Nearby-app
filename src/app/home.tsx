123dcefeaebeabfd297c5d374c62c6b0912dcb94
brunotxrs <elielsonbrunno@gmail.com>
2024-12-17 09:32:01 -0400

    feat: importing MapView

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { View, Alert } from "react-native";
+import MapView from "react-native-maps";
+import * as Location from 'expo-location';
 
 import { api } from "@/services/api";
 
@@ -7,8 +9,12 @@ import { Places } from "@/components/places";
 import { PlaceProps } from "@/components/place";
 import { Categories, CategoriesProps } from "@/components/categories";
 
-
 type MarketsProps = PlaceProps & {}
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+}
 
 export default function Home() {
     const [categories, setCategories] = useState<CategoriesProps>([])
@@ -40,6 +46,19 @@ export default function Home() {
         }
     }
 
+    async function getCurrentLocation() {
+        try {
+            const { granted } = await Location.requestForegroundPermissionsAsync();
+
+            if(granted){
+              const location = await Location.getCurrentPositionAsync();
+              console.log(location)
+            }
+        } catch (error) {
+            console.log(error)   
+        }
+    }
+
     useEffect(() => {
         fetchCategories()
     }, [])
@@ -50,7 +69,20 @@ export default function Home() {
 
     return(
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
-        <Categories data={categories}  onSelect={setCategory} selected={category}/>
+        <Categories
+         data={categories}
+         onSelect={setCategory}
+         selected={category}
+        />
+
+        <MapView style={{ flex: 1}}
+        initialRegion={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+        }}
+        />
 
         <Places data={markets} />
     </View>
