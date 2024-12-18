fd08dd7eff27fd9f9acfefdcd3c389f1534e85b9
brunotxrs <elielsonbrunno@gmail.com>
2024-12-18 10:06:53 -0400

    feat: importing the coupon component

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -4,6 +4,7 @@ import { router, useLocalSearchParams, Redirect } from "expo-router"
 
 import { Loading } from "@/components/loading"
 import { Cover } from "@/components/market/cover";
+import { Coupon } from "@/components/market/coupon";
 import { Details, PropsDetails } from "@/components/market/details";
 
 import { api } from "@/services/api"
@@ -53,6 +54,7 @@ export default function Market(){
         <View style={{ flex: 1 }}>
             <Cover uri={data?.cover} />
             <Details data={data} />
+            <Coupon code="FM4345T6" />
         </View>
     )
 }
